020be27a233f22e3764d6a43f0d838ed80295233
Thais <ferreira.thais98@hotmail.com>
2023-12-26 17:25:16 -0300

    Curso PHP

---
+++ desenvolvimento_php/incorporando_scripts/inicio.php
@@ -0,0 +1,14 @@
+<?php 
+    include_once "menu.php"; //warning(alerta)
+    
+    //require "menu2.php"; //fatal error(interrompe o resto do código)
+?>
+conteúdo da Página(Início)
+<br />
+
+<?php 
+    include_once "menu.php"; //warning
+
+    //require "menu2.php"; //fatal error
+?>
+conteúdo da Página(Início)
